7705a23be20f88bf635d834a37de1450b613436f
Angel-of-F3ar <marshburnraymond@gmail.com>
2025-07-11 20:25:43 -0500

    E: added each character count

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from stats import words_in_books, get_book_text
+from stats import words_in_books, get_book_text, all_char
 
 def main():
   text = get_book_text()
   num_words = words_in_books(text)
+  num_letters = all_char(text)
   print(f"{num_words} words found in the document")
+  print(num_letters)
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,14 @@ def get_book_text():
 def words_in_books(words):
   num_of_words = len(words.split())
   return num_of_words
+
+def all_char(words):
+  chars = {}
+  for char in words:
+    low_char = char.lower()
+    if low_char in chars:
+      #update count
+      chars[low_char] += 1
+    else:
+      chars[low_char] = 1
+  return chars
