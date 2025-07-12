a7823562552b15f4423b68f8de38e0583e30d5c5
Angel-of-F3ar <marshburnraymond@gmail.com>
2025-07-11 19:04:01 -0500

    C: created two functions for txt and wrd num

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text():
+  with open("books/frankenstein.txt") as f:
+    return f.read()
+
+def words_in_books(words):
+  num_of_words = len(words.split())
+  return num_of_words
+  
+def main():
+  text = get_book_text()
+  num_words = words_in_books(text)
+  print(f"{num_words} words found in the document")
+
+main()
